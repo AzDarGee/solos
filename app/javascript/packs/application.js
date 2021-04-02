6941feeafa320d5890562ac954f570148772d7e6
ASHISH DARJI <ashdarji1@gmail.com>
2021-04-02 16:27:33 -0400

    attempting to fix javascript in webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,38 +7,21 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'mdbootstrap/scss/mdb-free'
+// import 'mdbootstrap/css/mdb'
 // import 'mdbootstrap/js/mdb'
-// require("../stylesheets/application.scss")
 import "@fortawesome/fontawesome-free/css/all"
-import '../src/bootstrap.js'
+// import 'src/bootstrap.js'
+import "bootstrap";
 
 // Import MapBox & Map
-import mapboxgl from "mapbox-gl"
-mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 import "mapbox-gl/dist/mapbox-gl.css"
-import "../src/map.js"
 
 // Import Images
 require.context('../images', true)
-
 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-$(document).on('turbolinks:load', function() {
-    $('body').tooltip({
-      selector: '[data-toggle="tooltip"]',
-      container: 'body',
-    });
-  
-    $('body').popover({
-      selector: '[data-toggle="popover"]',
-      container: 'body',
-      html: true,
-      trigger: 'hover',
-    });
-  });
 
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,4 +1,5 @@
-
+import mapboxgl from "mapbox-gl"
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 $( document ).on('turbolinks:load', function() {
  
