e179a002104daa0bd8b2f959c884b48312fe780c
ASHISH DARJI <ashdarji1@gmail.com>
2021-04-08 14:40:24 -0400

    updating styling for registrations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ import "channels"
 
 import "@fortawesome/fontawesome-free/css/all"
 // import '../src/bootstrap.js'
-import {Alert} from "bootstrap";
-
+// import {Alert} from "bootstrap";
+import "bootstrap"
 
 // Import MapBox & Map
 import "mapbox-gl/dist/mapbox-gl.css"
